d3b816bc84dbbcdec93f6fc538da6bc66d0154cd
Branford Coleman <77608551+galaxy1027@users.noreply.github.com>
2024-01-02 04:16:51 -0800

    Initial commit

---
+++ nvim/lua/galaxy/lazy.lua
@@ -0,0 +1,38 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+    {
+        'nyngwang/nvimgelion',
+        priority = 1000, --load this before other plugins
+        config = function ()
+            -- load the colorscheme
+            vim.cmd([[colorscheme nvimgelion]])
+        end,
+    },
+    {
+        'nvim-telescope/telescope.nvim', tag = '0.1.5',
+        -- or                              , branch = '0.1.x',
+        dependencies = { 'nvim-lua/plenary.nvim' }
+    },
+    {
+        'nvim-treesitter/nvim-treesitter',
+        build = ":TSUpdate",
+    },
+    {
+        'mbbill/undotree',
+    },
+    {
+        'tpope/vim-fugitive',
+    },
+})
